9d05cefb693aacfcea94fafad9c6b92e95d3fafb
ayunami2000 <spwilliamsiam@gmail.com>
2018-01-17 11:22:03 -0500

    Update gamepadtest.js

--- gamepadtest.js
+++ gamepadtest.js
@@ -1,11 +1,3 @@
-/*
- * Gamepad API Test
- * Written in 2013 by Ted Mielczarek <ted@mielczarek.org>
- *
- * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
- *
- * You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
- */
 window.GamepadBtns={};
 window.GamepadBtns.xinput=["A","B","X","Y","LB","RB","LT","RT","BACK","START","LS","RS","UP","DOWN","LEFT","RIGHT","HOME"];
 var haveEvents = 'GamepadEvent' in window;
