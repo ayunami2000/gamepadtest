1be040d0f8b6a2ce805bca49012277ff9dcd162d
ayunami2000 <spwilliamsiam@gmail.com>
2018-01-17 11:25:42 -0500

    Update gamepadtest.js

--- gamepadtest.js
+++ gamepadtest.js
@@ -28,10 +28,10 @@ function addgamepad(gamepad) {
   for (var i=0; i<gamepad.buttons.length; i++) {
     var e = document.createElement("input");
     e.className="button";
-    e.placeholder=gamepad.id=="xinput"?window.GamepadBtns.xinput[i]:gamepad.buttons.length===17?window.GamepadBtns.xinput[i]:i;
+    e.placeholder=gamepad.id=="xinput"?window.GamepadBtns.xinput[i]:gamepad.buttons.length===(16||17)?window.GamepadBtns.xinput[i]:i;
     b.appendChild(e);
-    b.innerHTML+=i%2==0?"":(gamepad.id=="xinput"||gamepad.buttons.length===17)&&i===13?"":"<br>";
-    b.innerHTML+=(gamepad.id=="xinput"||gamepad.buttons.length===17)&&i===(3||7||9||11)?"<br>":"";
+    b.innerHTML+=i%2==0?"":(gamepad.id=="xinput"||gamepad.buttons.length===(16||17))&&i===13?"":"<br>";
+    b.innerHTML+=(gamepad.id=="xinput"||gamepad.buttons.length===(16||17))&&i===(3||7||9||11)?"<br>":"";
   }
   d.appendChild(b);
   var a = document.createElement("div");
